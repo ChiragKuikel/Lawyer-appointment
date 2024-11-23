30c3e3dde4b446ea6f07850aa0ed13f281bce237
Chirag Kuinkel <chiragkuinkeldrive3@gmail.com>
2024-11-23 17:33:39 +0545

    update 3rd

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,22 +5,22 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center text-sm py-4 mb-5 border-b border-b-gray-400'>
         <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
-        <ul >
-            <NavLink>
-                <li>HOME</li>
-                <hr />
+        <ul className='hidden md:flex items-start gap-5 font-medium'>
+            <NavLink to='/'>
+                <li className='py-1'>HOME</li>
+                <hr className='border-none out;ine-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
-            <NavLink>
-                <li>All DOCTORS</li>
-                <hr />
+            <NavLink to='/doctors'>
+                <li className='py-1'>All DOCTORS</li>
+                <hr className='border-none out;ine-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
-            <NavLink>
-                <li>ABOUT</li>
-                <hr />
+            <NavLink to='/about'>
+                <li className='py-1'>ABOUT</li>
+                <hr className='border-none out;ine-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
-            <NavLink>
-                <li>CONTACT</li>
-                <hr />
+            <NavLink to='/contact'>
+                <li className='py-1'>CONTACT</li>
+                <hr className='border-none out;ine-none h-0.5 bg-primary w-3/5 m-auto hidden'/>
             </NavLink>
         </ul>
         <div>
